d9ca16f34cdf9968ff5d0f92cc1ba8f181443d8b
Tori Lew <torirlew@gmail.com>
2022-10-07 09:47:16 -0700

    Completes all requested CONTROLLER and MODEL changes

--- app/src/main/java/com/zybooks/pizzaparty/PizzaCalculator.kt
+++ app/src/main/java/com/zybooks/pizzaparty/PizzaCalculator.kt
@@ -2,9 +2,9 @@ package com.zybooks.pizzaparty
 
 import kotlin.math.ceil
 
-const val SLICES_PER_PIZZA = 8
+// const val SLICES_PER_PIZZA = 8
 
-class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel) {
+class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel, var pizzaSize: PizzaSize) {
     var partySize = 0
         set(value) {
             field = if (value >= 0) value else 0
@@ -14,9 +14,13 @@ class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel) {
         LIGHT(2), MEDIUM(3), RAVENOUS(4)
     }
 
+    enum class PizzaSize(var pizzaSlices: Int) {
+        SMALL(6), MED(8), LARGE(12)
+    }
+
     val totalPizzas: Int
         get() {
-            return ceil(partySize * hungerLevel.numSlices / SLICES_PER_PIZZA.toDouble()).toInt()
+            return ceil(partySize * hungerLevel.numSlices / pizzaSize.pizzaSlices.toDouble()).toInt()
         }
 
     init {
