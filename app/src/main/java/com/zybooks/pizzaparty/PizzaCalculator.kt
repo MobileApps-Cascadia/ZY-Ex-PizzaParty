5b75f2240cca2088b48c5171cad6b305139654ef
Kenny <kennypluchau@gmail.com>
2022-10-10 18:05:09 -0700

    Created class to calculate pizza sizes based on user input

--- app/src/main/java/com/zybooks/pizzaparty/PizzaCalculator.kt
+++ app/src/main/java/com/zybooks/pizzaparty/PizzaCalculator.kt
@@ -2,9 +2,9 @@ package com.zybooks.pizzaparty
 
 import kotlin.math.ceil
 
-const val SLICES_PER_PIZZA = 8
+//const val SLICES_PER_PIZZA = 8
 
-class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel) {
+class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel, var pisize: SlicesPerPizza) {
     var partySize = 0
         set(value) {
             field = if (value >= 0) value else 0
@@ -13,10 +13,12 @@ class PizzaCalculator(partySize: Int, var hungerLevel: HungerLevel) {
     enum class HungerLevel(var numSlices: Int) {
         LIGHT(2), MEDIUM(3), RAVENOUS(4)
     }
-
+    enum class SlicesPerPizza(var pisize: Int) {
+        Small(6), Medium(8), Large(12)
+    }
     val totalPizzas: Int
         get() {
-            return ceil(partySize * hungerLevel.numSlices / SLICES_PER_PIZZA.toDouble()).toInt()
+            return ceil(partySize * hungerLevel.numSlices/ pisize.pisize.toDouble()).toInt()
         }
 
     init {
